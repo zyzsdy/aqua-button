bcd69980885842bc1c7b5db9f885bd94b2ce1d11
AD1024 <dh63@cs.washington.edu>
2020-07-21 15:05:46 -0700

    [ init ] from aqua-button

---
+++ src/components/modal.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <h4 class="modal-title" id="myModalLabel">{{$t(title)}}</h4>
+                </div>
+                <div class="modal-body">
+                    {{$t(msg)}}
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">{{$t("action.close")}}</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+.modal{
+    color: black;
+}
+</style>
+
+
+<script>
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import $ from 'jquery'
+
+@Component
+class Modal extends Vue {
+    title = "info.info";
+    msg = "info.null";
+    created(){
+        this.$gConst.globalbus.$on("send-info", (info) => {
+            this.msg = info;
+            this.showModal();
+        });
+    }
+    showModal(){
+        $("#myModal").modal();
+    }
+}
+export default Modal;
+</script>
